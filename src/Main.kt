f032151680796e26f9311707b8bc6d5ddd64f8c9
Richard Colvin <rxcolvin@yahoo.co.uk>
2017-08-08 08:37:14 +0100

    share

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.browser.document
+
 /**
  * Created by richard on 04/03/2017.
  */
@@ -6,4 +8,6 @@
 fun main(args: Array<String>) {
     val message = "Hello JavaScript!"
     println(message)
+
+    val element = document.getElementById("2")
 }
