8b25c00d23d6f81b5e76eab1900bb060f854a3b4
Richard Colvin <rxcolvin@yahoo.co.uk>
2017-08-08 07:49:21 +0100

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+/**
+ * Created by richard on 04/03/2017.
+ */
+
+
+fun main(args: Array<String>) {
+    val message = "Hello JavaScript!"
+    println(message)
+}
---
+++ src/uibrowser/UiBrowser.kt
@@ -0,0 +1,5 @@
+package uibrowser
+
+/**
+ * Created by richard on 08/08/2017.
+ */
